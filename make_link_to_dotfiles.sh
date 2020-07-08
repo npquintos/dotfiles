3635dd33b1d545fe5f072fcedb2b63ff4c9986e8
Noel Quintos <npquintos@gmail.com>
2020-07-08 11:43:25 -0600

    Update make_link_to_dotfiles.sh

--- make_link_to_dotfiles.sh
+++ make_link_to_dotfiles.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# use this when you are done with the installation
+# i.e. you already run install_my_fave_apps_and_dots.sh
+# and you have added a new user and would like that user
+# to inherit these dot files. This is made available for
+# anyone to use, placed in /usr/local/bin
+
 myhome="/home/$(whoami)"
 mysource="/etc/skel/dotfiles"
 for i in $(ls ${mysource}/__*); do
